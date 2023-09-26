5a7163a1d102dd163f801608b9abb19bd7201c27
Husinyuszi <husinyuszi13@gmail.com>
2023-09-26 21:04:17 +0200

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+class TestScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+        Debug.Log($"Hello {name}");
+        //változók
+        int myFirstVariable = 12;
+        
+    }
+}
+
